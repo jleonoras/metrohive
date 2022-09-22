582c41731a34da95eace2c6a971786d0afc67aec
John Leonoras <john.emberga.leonoras@gmail.com>
2022-09-23 01:25:00 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const port = 5000
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "success"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
