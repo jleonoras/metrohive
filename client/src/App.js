fb4826a1bd0d1ac21d666b0ac370b14edc08489e
John Leonoras <john.emberga.leonoras@gmail.com>
2022-09-23 18:09:26 +0800

    update main

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
-
-import Login from "./component/login/Login";
-// import Main from "./component/main/Main";
+import Main from "./component/main/Main";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <h1>Welcome</h1>
-      <Login/>
+      <Main/>
     </div>
   );
 }
--- client/src/component/main/Main.jsx
+++ client/src/component/main/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Login from '../login/Login'
 
 const Main = () => {
   return (
     <div>
-        Login
+      <h1>Welcome</h1>
+      <Login/>
     </div>
   )
 }
