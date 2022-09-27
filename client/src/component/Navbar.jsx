4677774112068574ddd33e0769eb6cf4570fd8ba
John Leonoras <john.emberga.leonoras@gmail.com>
2022-09-27 22:31:42 +0800

    ...

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- client/src/component/StyledNavbar.jsx
+++ client/src/component/StyledNavbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/login">Login</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Login</NavLink>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
