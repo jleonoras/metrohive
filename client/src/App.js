4bd969623fbcb0be8105b10ad1c3e2d1d87a25c8
John Leonoras <john.emberga.leonoras@gmail.com>
2022-10-19 19:30:30 +0800

    Add Bootstrap for navigation and mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,22 @@ import Home from "./page/Home";
 import Login from "./page/Login";
 import Error from "./page/Error";
 import "./component/nav.css";
-import SharedLayout from "./page/SharedLayout";
 import Register from "./page/Register";
+import Navbar from "./component/StyledNavbar";
+import AddListing from "./page/AddListing";
+import Listing from "./page/Listing";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Error />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/addlisting" element={<AddListing />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/page/Listing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Listing = () => {
+  return (
+    <div className="App-header">
+      <h1>Listing</h1>
+      <p>This is the Listing Page</p>
+    </div>
+  );
+};
+
+export default Listing;
