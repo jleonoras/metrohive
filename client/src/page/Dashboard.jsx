ad0248ef6346edc84a8e98cf0967f343f1d1b20f
John Leonoras <john.emberga.leonoras@gmail.com>
2022-10-25 23:54:50 +0800

    Add API route for user profile (back-end) and integrate to profile screen (front-end)

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
 
-const Dashboard = () => {
+const PROFILE_URL = "/api/v1/verify";
+
+const Dashboard = ({ setAuth }) => {
+  const [fname, setFName] = useState("");
+  const [lname, setLName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const getProfile = async () => {
+    try {
+      const response = await axios.get(PROFILE_URL, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      const parseRes = await response?.data;
+      setFName(parseRes.fname);
+      setLName(parseRes.lname);
+      setEmail(parseRes.email);
+      console.log(parseRes);
+    } catch (error) {
+      console.error(error.message);
+      console.log(error?.response);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <section>
       <div className="App-header">
         <h1>Dashboard</h1>
         <p>This is the Dashboard page.</p>
+        <h5>
+          {fname} {lname} <br />
+        </h5>
+        <h5>{email}</h5>
       </div>
     </section>
   );
