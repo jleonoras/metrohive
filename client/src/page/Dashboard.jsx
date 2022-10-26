95520fe9e0275207a34a9118864b6a1610430ade
John Leonoras <john.emberga.leonoras@gmail.com>
2022-10-27 00:32:22 +0800

    Add private route to Dashboard page

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 
-const PROFILE_URL = "/api/v1/verify";
+const PROFILE_URL = "/api/v1/profile";
 
 const Dashboard = ({ setAuth }) => {
   const [fname, setFName] = useState("");
   const [lname, setLName] = useState("");
   const [email, setEmail] = useState("");
+  const [welcomeMsg, setWelcomeMsg] = useState("");
 
   const getProfile = async () => {
     try {
@@ -20,7 +21,8 @@ const Dashboard = ({ setAuth }) => {
       setFName(parseRes.fname);
       setLName(parseRes.lname);
       setEmail(parseRes.email);
-      console.log(parseRes);
+      setWelcomeMsg("Welcome!");
+      // console.log(parseRes);
     } catch (error) {
       console.error(error.message);
       console.log(error?.response);
@@ -36,6 +38,7 @@ const Dashboard = ({ setAuth }) => {
       <div className="App-header">
         <h1>Dashboard</h1>
         <p>This is the Dashboard page.</p>
+        <h3>{welcomeMsg}</h3>
         <h5>
           {fname} {lname} <br />
         </h5>
