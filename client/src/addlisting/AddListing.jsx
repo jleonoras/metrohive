a699b15ca12186ab5c14f2ac5cf64816eeb5c58e
John Leonoras <john.emberga.leonoras@gmail.com>
2022-11-03 00:41:48 +0800

    Create add listing screen

--- client/src/addlisting/AddListing.jsx
+++ client/src/addlisting/AddListing.jsx
@@ -5,6 +5,35 @@ const AddListing = () => {
     <div className="App-header">
       <h1>Add Listing</h1>
       <p>This is the Add Listing page.</p>
+      <form encType="multipart/form-data">
+        <label>Description:</label>
+        <br />
+        <textarea
+          type="text"
+          name="description"
+          rows="2"
+          cols="25"
+          required
+          autoFocus
+        ></textarea>
+        <br />
+        <label>Price:</label>
+        <br />
+        <input type="number" name="price" required />
+        <br />
+        <label>Add Image:</label>
+        <br />
+        <input
+          type="file"
+          alt=""
+          name="image"
+          accept="image/*"
+          multiple
+          required
+        />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
