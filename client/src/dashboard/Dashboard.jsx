c760e405a6682ba1f47ad3fe06ece86d3785d5b9
John Leonoras <john.emberga.leonoras@gmail.com>
2022-11-05 02:16:35 +0800

    Attached the user listing component

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
+import UserListing from "../userListing/UserListing";
 
 const PROFILE_URL = "/api/v1/profile";
 
@@ -7,7 +8,6 @@ const Dashboard = ({ setAuth }) => {
   const [fname, setFName] = useState("");
   const [lname, setLName] = useState("");
   const [email, setEmail] = useState("");
-  const [welcomeMsg, setWelcomeMsg] = useState("");
 
   const getProfile = async () => {
     try {
@@ -21,7 +21,6 @@ const Dashboard = ({ setAuth }) => {
       setFName(parseRes.fname);
       setLName(parseRes.lname);
       setEmail(parseRes.email);
-      setWelcomeMsg("Welcome!");
       // console.log(parseRes);
     } catch (error) {
       console.error(error.message);
@@ -34,17 +33,21 @@ const Dashboard = ({ setAuth }) => {
   }, []);
 
   return (
-    <section>
-      <div className="App-header">
-        <h1>Dashboard</h1>
-        <p>This is the Dashboard page.</p>
-        <h3>{welcomeMsg}</h3>
-        <h5>
-          {fname} {lname} <br />
-        </h5>
-        <h5>{email}</h5>
-      </div>
-    </section>
+    <>
+      <section className="App">
+        <div className="App-header">
+          <h1>Dashboard</h1>
+          <p>This is the Dashboard page.</p>
+          <div>
+            <h2>
+              {fname} {lname}
+            </h2>
+          </div>
+          <h5>{email}</h5>
+          <UserListing />
+        </div>
+      </section>
+    </>
   );
 };
 
