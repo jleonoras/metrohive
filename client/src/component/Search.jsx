e5cf7f80a0bc9254020bc842facfaa4390c782a9
John Leonoras <john.emberga.leonoras@gmail.com>
2022-11-30 00:00:08 +0800

    Minor change in input form

--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -1,28 +1,33 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const navigate = useNavigate();
-  const queryRef = useRef(null);
+  // const queryRef = useRef(null);
+  const [query, setQuery] = useState("");
 
   const handleQuery = (e) => {
-    const location = queryRef.current.value;
+    // const location = queryRef.current.value;
+    e.preventDefault();
+
+    const location = query;
     navigate(`/listing/search/${location}`);
   };
 
   return (
     <div>
-      <label htmlFor="location"></label>
-      <input
-        type="text"
-        name="location"
-        ref={queryRef}
-        placeholder="Search location..."
-        required
-      ></input>
-      <button type="submit" onClick={handleQuery}>
-        Search
-      </button>
+      <form onSubmit={handleQuery}>
+        <label htmlFor="location"></label>
+        <input
+          type="text"
+          name="location"
+          value={query}
+          placeholder="Search location..."
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        ></input>
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 };
