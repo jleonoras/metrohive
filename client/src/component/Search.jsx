0a8ccc08c08d622cdd5b1ab3535886d99a8247b2
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-06 02:35:21 +0800

    Minor code changes in searchbar component

--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -15,22 +15,24 @@ const Search = () => {
   };
 
   return (
-    <div className="container p-5 bd-highlight d-inline-block">
-      <form className="input-group" onSubmit={handleQuery}>
-        <label htmlFor="location"></label>
-        <input
-          type="search"
-          name="location"
-          value={query}
-          placeholder="Search location..."
-          onChange={(e) => setQuery(e.target.value)}
-          required
-          className="form-control"
-        ></input>
-        <button type="submit button" className="btn btn-outline-secondary">
-          Search
-        </button>
-      </form>
+    <div className="container p-5">
+      <div className="container w-50">
+        <form className="input-group shadow-sm" onSubmit={handleQuery}>
+          <label htmlFor="location"></label>
+          <input
+            type="search text"
+            name="location"
+            value={query}
+            placeholder="Search location..."
+            onChange={(e) => setQuery(e.target.value)}
+            required
+            className="form-control"
+          ></input>
+          <button type="submit button" className="btn btn-outline-secondary">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
