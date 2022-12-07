b04497ffec46d6e0cb71d6b921aa9fbc96f10342
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-07 20:05:36 +0800

    Design the search listing result page.

--- client/src/result/Result.jsx
+++ client/src/result/Result.jsx
@@ -62,56 +62,143 @@ const Result = () => {
       .join(" ");
   };
 
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
   return (
-    <section className="App">
-      <div className="App-header">
+    <section>
+      <div className="container py-5">
         <div>
-          <ul>
+          <h6>
+            Search Result for "<strong>{toTitleCase(location)}</strong>"
+          </h6>
+        </div>
+        <div>
+          <ul className="row row-cols-1 row-cols-md-4 g-4 list-unstyled">
             {searchListing.length !== 0 && searchListing.listing_id !== null ? (
               searchListing.map((item, index) => {
                 return (
-                  <li
-                    key={index}
-                    onClick={() => {
-                      handleListingSelect(item.listing_id);
-                    }}
-                  >
-                    <div>
-                      <h3>Search Result for "{toTitleCase(location)}"</h3>
-                    </div>
-                    <figure>
-                      <img
-                        src={item.image1}
-                        alt={item.description}
-                        loading="lazy"
-                      ></img>
-                      <img
-                        src={item.image2}
-                        alt={item.description}
-                        loading="lazy"
-                      ></img>
-                      <img
-                        src={item.image3}
-                        alt={item.description}
-                        loading="lazy"
-                      ></img>
-                    </figure>
-                    <div>
-                      <h3>Price:</h3>
-                      <p>₱{item.price}</p>
-                    </div>
-                    <div>Description:</div>
-                    <p>{item.description}</p>
-                    <div>
-                      <h3>Location:</h3>
-                      <p>{item.location}</p>
+                  <li className="col" key={index}>
+                    <div className="card h-100 shadow-sm">
+                      <figure className="figure">
+                        <div
+                          id={`carouselImage-${item.listing_id}`}
+                          className="carousel slide"
+                          data-bs-ride="false"
+                        >
+                          <div className="carousel-indicators">
+                            <button
+                              type="button"
+                              data-bs-target={`#carouselImage-${item.listing_id}`}
+                              data-bs-slide-to="0"
+                              className="active"
+                            ></button>
+                            <button
+                              type="button"
+                              data-bs-target={`#carouselImage-${item.listing_id}`}
+                              data-bs-slide-to="1"
+                            ></button>
+                            <button
+                              type="button"
+                              data-bs-target={`#carouselImage-${item.listing_id}`}
+                              data-bs-slide-to="2"
+                            ></button>
+                          </div>
+                          <div className="carousel-inner">
+                            <div className="carousel-item active ratio ratio-4x3">
+                              <img
+                                src={item.image1}
+                                alt={item.description}
+                                loading="lazy"
+                                className="card-img-top d-block w-100 img-fluid"
+                              ></img>
+                            </div>
+                            <div className="carousel-item ratio ratio-4x3">
+                              <img
+                                src={item.image2}
+                                alt={item.description}
+                                loading="lazy"
+                                className="card-img-top d-block w-100 img-fluid"
+                              ></img>
+                            </div>
+                            <div className="carousel-item ratio ratio-4x3">
+                              <img
+                                src={item.image3}
+                                alt={item.description}
+                                loading="lazy"
+                                className="card-img-top d-block w-100 img-fluid"
+                              ></img>
+                            </div>
+                            {/* <!-- Controls --> */}
+                            <button
+                              className="carousel-control-prev"
+                              type="button"
+                              data-bs-target={`#carouselImage-${item.listing_id}`}
+                              data-bs-slide="prev"
+                            >
+                              <span
+                                className="carousel-control-prev-icon"
+                                aria-hidden="true"
+                              ></span>
+                              <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button
+                              className="carousel-control-next"
+                              type="button"
+                              data-bs-target={`#carouselImage-${item.listing_id}`}
+                              data-bs-slide="next"
+                            >
+                              <span
+                                className="carousel-control-next-icon"
+                                aria-hidden="true"
+                              ></span>
+                              <span className="visually-hidden">Next</span>
+                            </button>
+                          </div>
+                        </div>
+                      </figure>
+                      <div
+                        type="button"
+                        className="card-body"
+                        onClick={() => {
+                          handleListingSelect(item.listing_id);
+                        }}
+                      >
+                        <div>
+                          <div>
+                            <strong>
+                              {new Intl.NumberFormat("en-PH", {
+                                currency: "PHP",
+                                style: "currency",
+                              }).format(`${item.price}`)}
+                            </strong>
+                          </div>
+                          <p>{item.description}</p>
+                          <div>
+                            <strong>{item.location}</strong>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </li>
                 );
               })
             ) : (
-              <div>
-                <h3>No Result for "{toTitleCase(location)}"</h3>
+              <div className="container py-5">
+                <div className="d-flex justify-content-center align-items-center">
+                  <span>
+                    No Result for "<strong>{toTitleCase(location)}</strong>"
+                  </span>
+                </div>
+                <div
+                  className="d-flex justify-content-center align-items-center py-3"
+                  onClick={handleBackToHome}
+                >
+                  <button className="btn btn-warning bg-gradient" type="button">
+                    Back to Home
+                  </button>
+                </div>
               </div>
             )}
           </ul>
