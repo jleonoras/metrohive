ec843ec3ad434efe80c05489f4264d4227a4dd8f
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-14 18:55:23 +0800

    Change the nav background from dark to light.

--- client/src/component/StyledNavbar.jsx
+++ client/src/component/StyledNavbar.jsx
@@ -3,12 +3,12 @@ import { Link, NavLink } from "react-router-dom";
 
 const Topnav = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-dark bg-gradient p-3 sticky-top shadow">
+    <nav className="navbar navbar-expand-md navbar-light bg-light bg-gradient p-3 sticky-top shadow-sm">
       <div className="container-md" id="topnav">
         <Link className="navbar-brand mb-0 h1 text-danger" to="/">
           <strong>Metrohyve</strong>
         </Link>
-        <div className="bg-secondary bg-gradient rounded">
+        <div className="bg-light bg-gradient rounded">
           <button
             className="navbar-toggler"
             data-bs-toggle="collapse"
@@ -26,19 +26,19 @@ const Topnav = () => {
         >
           <ul className="navbar-nav nav-pills mx-2">
             <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/" end>
+              <NavLink className="nav-link text-secondary" to="/" end>
                 Home
               </NavLink>
             </li>
           </ul>
           <ul className="navbar-nav nav-pills mx-2 add-listing">
             <li className="nav-item ">
-              <NavLink className="nav-link text-light" to="/add-listing">
+              <NavLink className="nav-link text-secondary" to="/add-listing">
                 Add Listing
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/dashboard">
+              <NavLink className="nav-link text-secondary" to="/dashboard">
                 Account
               </NavLink>
             </li>
