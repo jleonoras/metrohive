9f51d4686f6c8065ca8ea00011a4764f45698095
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-15 21:49:47 +0800

    Store the credentials in the .env file.

--- server/pool.js
+++ server/pool.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 const connectDatabase = () => {
   const pool = new pg.Pool({
-    user: "postgres",
+    user: process.env.dbUser,
     password: process.env.dbPassword,
     database: "metrohivedb",
     host: "localhost",
