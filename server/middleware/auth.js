2a73dcd3caf775de69ad27698f0f39adf0f6423e
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-24 05:08:14 +0800

    Minor code changes in handling token (set token to cookie)

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,14 +4,17 @@ dotenv.config();
 
 const auth = (request, response, next) => {
   //this will check if a token is existing in the Authorization Header
-  const token = request.header("Authorization");
+  // const token = request.header("Authorization");
+  const token = request.cookies.accessToken;
+
   if (!token) {
     return response.status(403).json({ error: "Invalid token" });
   }
   try {
     //this will take the token from the Authorization header then will use
     //jwt.verify function to process it
-    jwt.verify(token.slice(7), process.env.jwtSecret, (error, user) => {
+    // jwt.verify(token.slice(7), process.env.jwtSecret, (error, user) => {
+    jwt.verify(token, process.env.jwtSecret, (error, user) => {
       if (error) return response.sendStatus(403);
       request.user = user;
       next();
