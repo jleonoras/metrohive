99233d1c7f19c577f46571138976b357727043f3
John Leonoras <john.emberga.leonoras@gmail.com>
2022-12-27 14:54:36 +0800

    Minor code changes in error response

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
         setIsAuthenticated(parseRes);
       } catch (error) {
-        console.error(error.message);
+        console.log(error.response.data);
       }
     };
     checkAuthenticated();
