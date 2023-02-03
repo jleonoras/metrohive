5ff5479ca812b3c372ec40d179b41bbcdbc33b57
John Leonoras <john.emberga.leonoras@gmail.com>
2023-02-04 06:24:38 +0800

    Minor code changes in bookings table

--- client/src/bookedListing/BookedListingTable.jsx
+++ client/src/bookedListing/BookedListingTable.jsx
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 import BookingClass from "../booking/bookingClass";
-import { useParams } from "react-router-dom";
 
 const BOOKED_LISTING_API_URL = "/api/v1/booking";
 
-const BookedListingTable = () => {
+const BookedListingTable = ({ listingId }) => {
   const [bookedListing, setBookedListing] = useState([]);
-  const { id } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${BOOKED_LISTING_API_URL}/${id}`, {
-          withCredentials: true,
-          credentials: "include",
-          headers: {
-            Accept: "applicaiton/json",
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await axios.get(
+          `${BOOKED_LISTING_API_URL}/${listingId}`,
+          {
+            withCredentials: true,
+            credentials: "include",
+            headers: {
+              Accept: "applicaiton/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-        const parseRes = await response.data.booking;
+        const parseRes = await response?.data?.booking;
 
         const itemBookedListing = parseRes.map((item) => {
           return new BookingClass({
@@ -40,7 +42,7 @@ const BookedListingTable = () => {
       }
     };
     fetchData();
-  }, [id]);
+  }, [listingId]);
 
   const convertToMDY = (dateString) => {
     const date = new Date(dateString);
@@ -52,40 +54,38 @@ const BookedListingTable = () => {
   };
 
   return (
-    <section>
-      <div className="p-2 shadow">
-        <div className="table-responsive-md">
-          <table className="table table-striped table-hover">
-            <thead className="table-primary">
-              <tr>
-                <th scope="col">Date Reserved</th>
-                <th scope="col">Client Name</th>
-                <th scope="col">Client Email</th>
-                <th scope="col">Start Date</th>
-                <th scope="col">End Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bookedListing.length !== 0 &&
-                bookedListing[0].bookingId !== null &&
-                bookedListing.map((item) => {
-                  return (
-                    <tr key={item.bookedId}>
-                      <td>{convertToMDY(`${item.dateBooked}`)}</td>
-                      <td>
-                        {item.fname} {item.lname}
-                      </td>
-                      <td>{item.email}</td>
-                      <td>{convertToMDY(`${item.startDate}`)}</td>
-                      <td>{convertToMDY(`${item.endDate}`)}</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        </div>
+    <div className="p-2 shadow rounded bg-gradient bg-light">
+      <div className="table-responsive-md">
+        <table className="table table-striped table-hover">
+          <thead className="table-primary">
+            <tr>
+              <th scope="col">Date Reserved</th>
+              <th scope="col">Client Name</th>
+              <th scope="col">Client Email</th>
+              <th scope="col">Check-in</th>
+              <th scope="col">Check-out</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookedListing.length !== 0 &&
+              bookedListing[0].bookingId !== null &&
+              bookedListing.map((item) => {
+                return (
+                  <tr key={item.bookingId}>
+                    <td>{convertToMDY(`${item.dateBooked}`)}</td>
+                    <td>
+                      {item.fname} {item.lname}
+                    </td>
+                    <td>{item.email}</td>
+                    <td>{convertToMDY(`${item.startDate}`)}</td>
+                    <td>{convertToMDY(`${item.endDate}`)}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
-    </section>
+    </div>
   );
 };
 
