6b8156ed8689e296156ed661bf755308aa37a451
John Leonoras <john.emberga.leonoras@gmail.com>
2023-02-13 22:19:40 +0800

    Minor code changes in the declined booking table component

--- client/src/bookedListing/DeclinedBookingTable.jsx
+++ client/src/bookedListing/DeclinedBookingTable.jsx
@@ -1,49 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "../api/axios";
-import BookingClass from "../booking/bookingClass";
-
-const DECLINED_BOOKINGS_API_URL = "/api/v1/declined";
-
-const DeclinedBookingTable = ({ listingId }) => {
-  const [bookedListing, setBookedListing] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `${DECLINED_BOOKINGS_API_URL}/${listingId}`,
-          {
-            withCredentials: true,
-            credentials: "include",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        const parseRes = response.data.booking;
-
-        const itemBookedListing = parseRes.map((item) => {
-          return new BookingClass({
-            bookingId: item.booking_id,
-            dateBooked: item.date_booked,
-            fname: item.fname,
-            lname: item.lname,
-            email: item.email,
-            startDate: item.start_date,
-            endDate: item.end_date,
-            status: item.status,
-          });
-        });
-        setBookedListing(itemBookedListing);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [listingId]);
-
+const DeclinedBookingTable = ({ declinedBookings }) => {
   const convertToMDY = (dateString) => {
     const date = new Date(dateString);
     const month = date.getMonth() + 1;
@@ -74,15 +29,12 @@ const DeclinedBookingTable = ({ listingId }) => {
               <th scope="col" className="text-center">
                 Check-out
               </th>
-              <th scope="col" className="text-center">
-                Status
-              </th>
             </tr>
           </thead>
           <tbody>
-            {bookedListing.length !== 0 &&
-              bookedListing[0].bookingId !== null &&
-              bookedListing.map((item) => {
+            {declinedBookings.length !== 0 &&
+              declinedBookings[0].bookingId !== null &&
+              declinedBookings.map((item) => {
                 return (
                   <tr key={item.bookingId}>
                     <td className="text-center">
@@ -98,7 +50,6 @@ const DeclinedBookingTable = ({ listingId }) => {
                     <td className="text-center">
                       {convertToMDY(`${item.endDate}`)}
                     </td>
-                    <td className="text-center">{item.status}</td>
                   </tr>
                 );
               })}
