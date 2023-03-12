75e4a7fb7c2fa6c889d08a6e7bbdc433565a4685
John Leonoras <john.emberga.leonoras@gmail.com>
2023-03-12 23:08:51 +0800

    Minor code changes in axios instance custom config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,4 +4,8 @@ const port = 5000;
 
 export default axios.create({
   baseURL: `http://localhost:${port}`,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
